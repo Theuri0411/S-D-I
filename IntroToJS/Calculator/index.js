22e9f643cfda758b1876c7ed98bc8c2724e8b403
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-11-02 00:51:29 +0300

    update

--- IntroToJS/Calculator/index.js
+++ IntroToJS/Calculator/index.js
@@ -3,4 +3,12 @@ let num2 = 2
 
 document.getElementById ("num1-el").textContent = num1
 
-document,getElementById ("num2-el").textContent = num2
+document.getElementById ("num2-el").textContent = num2
+
+let sumEl = document.getElementById ("sum-el")
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+
+}
